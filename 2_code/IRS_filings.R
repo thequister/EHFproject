8f3c3ede6a44ea83017868fdcadfcb1df45f0c97
John Ahlquist <john.ahlquist@gmail.com>
2024-07-11 11:40:09 -0700

    IRS data coding

--- 2_code/IRS_filings.R
+++ 2_code/IRS_filings.R
@@ -7,6 +7,21 @@ library(irs990efile)
 library(curl)
 library(here)
 
+
+set.seed(1976)
+
+#helper fcns
+first_4_digits <- function(x) {
+  x_str <- as.character(x)
+  # Remove any non-digit characters (like decimal points)
+  x_str <- gsub("[^0-9]", "", x_str)
+  # Select the first 4 digits
+  first_4 <- as.numeric(substr(x_str, 1, 4))
+  return(first_4)
+}
+
+###
+
 #NTEE codes
 #M20 Disaster Preparedness and Relief Service
 #P60
@@ -22,19 +37,21 @@ library(here)
 # SEIU home health: 13-4129368  Y43
 
 #BMF download
-#update URL and filename if downloaded again
-bmfurl <- "https://nccsdata.s3.amazonaws.com/harmonized/bmf/unified/BMF_UNIFIED_V1.1.csv"
-destination <- here::here("0_raw_data", "IRS", "bmf_unified_v1_1_062624.csv")
+## downloaded 6/26/24
+##update URL and filename if downloaded again
+#bmfurl <- "https://nccsdata.s3.amazonaws.com/harmonized/bmf/unified/BMF_UNIFIED_V1.1.csv"
+#destination <- here::here("0_raw_data", "IRS", "bmf_unified_v1_1_062624.csv")
 
 # Download the data file
-curl_download(bmfurl, 
-  destfile = destination)
+#curl_download(bmfurl, 
+#  destfile = destination)
 
 #Download data dictionary
-bmfurl<- "https://nccsdata.s3.amazonaws.com/harmonized/harmonized_data_dictionary.xlsx"
-destination <- here::here("0_raw_data", "IRS", "bmf_datadictionary_062624.xlsx")
-curl_download(bmfurl, 
-  destfile = destination)
+## downloaded 6/26/24
+#bmfurl<- "https://nccsdata.s3.amazonaws.com/harmonized/harmonized_data_dictionary.xlsx"
+#destination <- here::here("0_raw_data", "IRS", "bmf_datadictionary_062624.xlsx")
+#curl_download(bmfurl, 
+#  destfile = destination)
 
 
 # need to redo by pulling multiple years from NCCS
@@ -42,6 +59,7 @@ curl_download(bmfurl,
 # finding unique EIN and then comparing that
 # list to the EIN list from irs990efile pz
 
+#comparing coverage over datasets for 2019
 test <- nccsdata::get_data(
   dsname = "core",
   scope.orgtype = "CHARITIES",
@@ -94,3 +112,89 @@ sum(index_pz_uein %in% compare_pz_uein)/norg_index_pz #75%
 
 length(intersect(index_uein,compare_pz_uein)) #324600
 length(intersect(index_pz_uein,compare_pz_uein)) #323846
+
+# NCCS data 2017
+## doing this sequentially, as downloading one big file
+## caused problems
+
+compare_pz_15 <- nccsdata::get_data(
+  dsname = "core",
+  scope.orgtype = "CHARITIES",
+  scope.formtype = "PZ",
+  time = c("2015")
+)
+
+compare_pz_16 <- nccsdata::get_data(
+  dsname = "core",
+  scope.orgtype = "CHARITIES",
+  scope.formtype = "PZ",
+  time = c("2016")
+)
+
+
+compare_pz_17 <- nccsdata::get_data(
+  dsname = "core",
+  scope.orgtype = "CHARITIES",
+  scope.formtype = "PZ",
+  time = c("2017")
+)
+
+compare_pz_18 <- nccsdata::get_data(
+  dsname = "core",
+  scope.orgtype = "CHARITIES",
+  scope.formtype = "PZ",
+  time = c("2018")
+)
+
+
+compare_pz_15 <- compare_pz_15 |> 
+  filter(
+    first_4_digits(TAXPER) == 2017) #0 2017 returns
+
+compare_pz_16 <- compare_pz_16 |> 
+  filter(
+    first_4_digits(TAXPER) == 2017)
+
+compare_pz_17 <- compare_pz_17 |> 
+  filter(
+    first_4_digits(TAXPER) == 2017)
+
+
+compare_pz_18 <- compare_pz_18 |> 
+  filter(
+    first_4_digits(TAXPER) == 2017)
+
+compare_pz_19 <- compare_pz |> 
+  filter(
+    first_4_digits(TAXPER) == 2017)
+
+compare_pz_ein_2017 <- unique(
+  c( compare_pz_16$EIN,
+     compare_pz_17$EIN,
+     compare_pz_18$EIN,
+     compare_pz_19$EIN)
+  )
+
+n_compare_pz_2017 <- length(compare_pz_ein_2017)
+
+index_2017 <- irs990efile::build_index( tax.years=2017 )
+
+n_index_2017 <- nrow(index_2017)
+n_index_2017_pz <- nrow(index_2017[index_2017$FormType %in% c("990", "990EZ"),])
+index_2017_uein <- unique(index_2017$EIN)
+index_2017_pz_uein <- unique(index_2017$EIN[index_2017$FormType %in% c("990", "990EZ")])
+norg_index_2017<- length(index_2017_uein)
+norg_index_2017_pz<-length(index_2017_pz_uein)
+
+sum(compare_pz_ein_2017 %in% index_2017_pz_uein)/n_compare_pz_2017 #74.4%
+sum(index_2017_pz_uein %in% compare_pz_ein_2017)/norg_index_2017_pz #70%
+
+length(intersect(index_2017_pz_uein,compare_pz_ein_2017)) #272482
+
+# BMF
+
+# 1st 10k records in BMF data
+bmf_10k<-data.table::fread(
+  file =here::here("0_raw_data", "IRS", "bmf_unified_v1_1_062624.csv"),
+  nrows = 10000
+  )
