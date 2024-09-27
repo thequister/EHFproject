ad809ae469c32711ccb39330682130d32cc7d47f
Theodoros Ntounias <tntounias@gmail.com>
2024-09-27 20:49:42 +0300

    video pretesting code

---
+++ 2_code/video_pretest.R
@@ -0,0 +1,20 @@
+## Analysis of video pretest
+## Conducted on Prolific using Qualtrics, Sep 2024
+
+library(here)
+library(tidyverse)
+library(readr)
+
+vid <- read_csv(here("0_raw_data", "Pretesting", "EHF+Survey+-+Walmart+-+Pretest+-+Video_September+23,+2024_08.59", "EHF Survey - Walmart - Pretest - Video_September 23, 2024_08.59.csv"))
+
+vid <- vid %>%
+  rename(age = Q7, description= Q199, duration = `Duration (in seconds)`, RealVid = VidTreatment) %>%
+  select(age, description, duration, RealVid) %>%
+  filter(!row_number() %in% c(1, 2)) %>%
+  mutate(age = as.numeric(age), duration = as.numeric(duration), RealVid = case_match(RealVid, "0" ~ "Home Depot", "1" ~ "Walmart"))
+
+vid %>% group_by(RealVid) %>%
+  summarize(mean_dur = mean(duration), mean_age = mean(age), 
+            median_dur = median(duration), median_age = median(age))
+
+print(vid[order(vid$RealVid), ])
