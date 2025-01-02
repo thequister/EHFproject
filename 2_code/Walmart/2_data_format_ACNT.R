161ea3ce4437091337082db0d4ef14226248a78f
Theodoros Ntounias <tntounias@gmail.com>
2025-01-02 15:16:14 -0800

    minor changes to data formatting

--- 2_code/Walmart/2_data_format_ACNT.R
+++ 2_code/Walmart/2_data_format_ACNT.R
@@ -2,3 +2,7 @@
 #source(here::here('2_code', 'Walmart', '1_libraries_and_settings_ACNT.R'))
 #here::i_am("2_code/Walmart/2_data_format_ACNT.R")
 
+ACNT_uw <- read_csv(here("0_raw_data", "ACNT", "ACNT_clean.csv"))
+
+ACNT_uw <- ACNT_uw %>%
+  mutate()
