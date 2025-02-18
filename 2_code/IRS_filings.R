4d362fffb6a8dc4a28eefb13850397cffa98b47c
John Ahlquist <john.ahlquist@gmail.com>
2025-02-17 18:17:46 -0800

    fixing conflicted files from laptop and home machine.

--- 2_code/IRS_filings.R
+++ 2_code/IRS_filings.R
@@ -76,6 +76,76 @@ ggsave(
 )
 
 
+### EHF outsourcers 
+
+outsourcers <-  readr::read_csv(
+  here::here("1_secondary_data", "EHFCompanyInfo", "EHF_outsourcers.csv")
+)
+
+outsourcers <- outsourcers |> mutate(
+  year = as_date(
+    paste(year,"-01-01", sep="")
+  ),
+  year = year(year)
+) |> 
+  filter(in_ehf==1)
+
+
+outsource_revenue_plot <-ggplot(outsourcers, 
+                     aes(x = as.integer(year), y = revenue/1000000, color = name,
+                         linetype = name)) +
+  geom_line(linewidth=1.2) +                      # Add lines to the plot
+  labs(title = "Revenues of major 3rd-party EHF managers",
+       x = "Year",
+       y = "Total revenue ($MM log scale)") +     # Add labels and legend title
+  scale_y_log10() +
+  theme_minimal() + 
+  theme(
+    plot.title = element_text(face = "bold", size = 16),
+    axis.title.x = element_text(size = 12),
+    axis.title.y = element_text(size = 12),
+    legend.title = element_blank()
+  )
+
+
+outsource_dgrant_plot <-ggplot(outsourcers, 
+                                aes(x = round(year,0), y = grants_paid_domestic/1000000, color = name,
+                                    linetype = name)) +
+  geom_line(linewidth=1.2) +                      # Add lines to the plot
+  labs(title = "Grants paid by major 3rd party EHF managers",
+       x = "Year",
+       y = "Grants paid to domestic individuals ($MM, log scale)") +               # Add labels and legend title
+  scale_y_log10() +
+  theme_minimal() + 
+  theme(
+    plot.title = element_text(face = "bold", size = 16),
+    axis.title.x = element_text(size = 12),
+    axis.title.y = element_text(size = 12),
+    legend.title = element_blank()
+  )
+
+outsource_fgrant_plot <-ggplot(outsourcers, 
+                               aes(x = round(year,0), y = grants_paid_foreign/1000000, color = name,
+                                   linetype = name)) +
+  geom_line(linewidth=1.2) +                      # Add lines to the plot
+  labs(title = "Grants paid by major 3rd party EHF managers",
+       x = "Year",
+       y = "Grants paid to foreign individuals ($MM log scale)") +               # Add labels and legend title
+  scale_y_log10() +
+  theme_minimal() + 
+  theme(
+    plot.title = element_text(face = "bold", size = 16),
+    axis.title.x = element_text(size = 12),
+    axis.title.y = element_text(size = 12),
+    legend.title = element_blank()
+  )
+
+
+
+
+
+
+
 ###
 
 #NTEE codes
