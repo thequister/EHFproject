0e5051515a870482010c53c54be7be9d33875046
Theodoros Ntounias <tntounias@gmail.com>
2025-05-04 15:34:18 -0700

    balance and descr tables

---
+++ 2_code/general_retail/7_descr_gr.R
@@ -0,0 +1,89 @@
+source(here::here('2_code', 'general_retail', '2_data_format_gr.R'))
+source(here::here('2_code', 'general_retail', '3_weights_gr.R'))
+
+### Descriptives Table
+
+# Unweighted
+tbl_sum_genpop_1<- 
+  gr |> 
+  select(ehf_aware_pretr, age_clean, male, main_job, 
+         tenure_fac, nonwhite, fulltime, hourly, college, member_union,
+         ehf_prior_know, emergency_expense, new_job, ehf_offer_percent, attachment_index) |> 
+  gtsummary::tbl_summary(missing_text = "(missing/not gathered)",
+                         label = list( 
+                           ehf_aware_pretr ~ "EHF awareness (pre-treatment)", 
+                           male ~ "male",
+                           main_job ~ "main job", tenure_fac ~ "job tenure",
+                           nonwhite ~ "PoC/nonwhite", fulltime ~ "full time",
+                           hourly ~ "hourly", college ~ "college degree", member_union ~ "union member",
+                           ehf_prior_know ~ "EHF awareness (post-treatment)", emergency_expense ~ "deal with emergency expense", 
+                           new_job ~ "accept new job", ehf_offer_percent ~ "accuracy of ehf ident", attachment_index ~ "attachment index"
+                         )
+  ) |>  
+  gtsummary::modify_caption("**Sample snames(ummary statistics by treatment group**") |> 
+  gtsummary::bold_labels() |> 
+  gtsummary::as_kable_extra()
+
+tbl_sum_genpop_1
+
+
+tbl_sum_genpop_2<- 
+  gr |> 
+  select(wrk_loyal, emp_loyal, emp_reco, 
+         union_vote, govt_responsib_hardship, govt_responsib_elder, govt_responsib_unemp) |>
+  gtsummary::tbl_summary(missing_text = "(missing/not gathered)",
+                         label = list(
+                           wrk_loyal ~"loyalty to coworkers", emp_loyal~"loyalty to employer",
+                           emp_reco~"recommend employer", union_vote ~"union vote", 
+                           govt_responsib_hardship ~"hardship support", govt_responsib_elder~"pension support", govt_responsib_unemp~"UI support"
+                         )
+  ) |> 
+  gtsummary::modify_caption("*summary table (cont'd)*") |> 
+  gtsummary::bold_labels() |> 
+  gtsummary::as_kable_extra()
+
+tbl_sum_genpop_2
+
+
+# Weighted
+gr_w <- gr_clean %>%
+  srvyr::as_survey_design(ids = 1, weights = acs_weight_trim)
+
+tbl_sum_genpop_1w<- 
+  gr_w |> 
+  select(ehf_aware_pretr, age_clean, male, main_job, 
+         tenure_fac, nonwhite, fulltime, hourly, college, member_union,
+         ehf_prior_know, emergency_expense, new_job, ehf_offer_percent, attachment_index) |> 
+  gtsummary::tbl_svysummary(missing_text = "(missing/not gathered)",
+                         label = list( 
+                           ehf_aware_pretr ~ "EHF awareness (pre-treatment)", 
+                           male ~ "male",
+                           main_job ~ "main job", tenure_fac ~ "job tenure",
+                           nonwhite ~ "PoC/nonwhite", fulltime ~ "full time",
+                           hourly ~ "hourly", college ~ "college degree", member_union ~ "union member",
+                           ehf_prior_know ~ "EHF awareness (post-treatment)", emergency_expense ~ "deal with emergency expense", 
+                           new_job ~ "accept new job", ehf_offer_percent ~ "accuracy of ehf ident", attachment_index ~ "attachment index"
+                         )
+  ) |>  
+  gtsummary::modify_caption("**Sample snames(ummary statistics by treatment group**") |> 
+  gtsummary::bold_labels() |> 
+  gtsummary::as_kable_extra()
+
+tbl_sum_genpop_1w
+
+tbl_sum_genpop_2w<- 
+  gr_w |> 
+  select(wrk_loyal, emp_loyal, emp_reco, 
+         union_vote, govt_responsib_hardship, govt_responsib_elder, govt_responsib_unemp) |>
+  gtsummary::tbl_svysummary(missing_text = "(missing/not gathered)",
+                         label = list(
+                           wrk_loyal ~"loyalty to coworkers", emp_loyal~"loyalty to employer",
+                           emp_reco~"recommend employer", union_vote ~"union vote", 
+                           govt_responsib_hardship ~"hardship support", govt_responsib_elder~"pension support", govt_responsib_unemp~"UI support"
+                         )
+  ) |> 
+  gtsummary::modify_caption("*summary table (cont'd)*") |> 
+  gtsummary::bold_labels() |> 
+  gtsummary::as_kable_extra()
+
+tbl_sum_genpop_2w
---
+++ 2_code/unemp_prelim.R
@@ -0,0 +1,81 @@
+library(here)
+source(here::here('2_code', '1_libraries_and_settings_global.R'))
+
+ACNT <- read_csv(here("3_cleaned_data", "ACNT_clean.csv")) %>%
+  select(unemp_benefits, other_welfare, ehf_aware_pretr, rk_wgt_trim) %>%
+  mutate(og = "ACNT") %>%
+  rename(wgt = rk_wgt_trim)
+
+gr <- read_csv(here("3_cleaned_data", "general_retail_clean.csv")) %>%
+  select(unemp_benefits, other_welfare, ehf_aware_pretr, ehf_support_exist, ehf_support_new, acs_weight_trim) %>%
+  mutate(og = "General Retail", 
+         new = ifelse(is.na(ehf_support_exist), "Support for a new EHF (Gen retail)", "Support for existing EHF (Gen retail)"), 
+         supp = ifelse(is.na(ehf_support_exist), ehf_support_new, ehf_support_exist)) %>%
+  rename(wgt = acs_weight_trim)
+
+THD <- read_csv(here("3_cleaned_data", "THD_clean.csv")) %>%
+  select(Q4.7, Q4.10, EHF_aware_list, rk_wgt) %>%
+  rename(unemp_benefits = Q4.7, other_welfare = Q4.10, ehf_aware_pretr = EHF_aware_list, wgt = rk_wgt) %>%
+  mutate(og = "THD")
+
+wf <- bind_rows(ACNT, gr, THD)
+
+## Correlations
+cor(as.numeric(ACNT$unemp_benefits == "Yes"), as.numeric(ACNT$ehf_aware_pretr)) # Some relationship here
+cor(as.numeric(gr$unemp_benefits == "Yes"), as.numeric(gr$ehf_aware_pretr))
+cor(as.numeric(THD$Q4.7 == "Yes"), as.numeric(THD$EHF_aware_list))
+
+cor.test(as.numeric(ACNT$unemp_benefits == "Yes"), as.numeric(ACNT$ehf_aware_pretr))
+cor.test(as.numeric(gr$unemp_benefits == "Yes"), as.numeric(gr$ehf_aware_pretr))
+cor.test(as.numeric(THD$Q4.7 == "Yes"), as.numeric(THD$EHF_aware_list))
+
+cor(as.numeric(ACNT$other_welfare == "Yes"), as.numeric(ACNT$ehf_aware_pretr)) # No relationship
+cor(as.numeric(gr$other_welfare == "Yes"), as.numeric(gr$ehf_aware_pretr))
+cor(as.numeric(THD$Q4.10 == "Yes"), as.numeric(THD$EHF_aware_list), use = "complete.obs")
+
+cor.test(as.numeric(ACNT$other_welfare == "Yes"), as.numeric(ACNT$ehf_aware_pretr))
+cor.test(as.numeric(gr$other_welfare == "Yes"), as.numeric(gr$ehf_aware_pretr))
+cor.test(as.numeric(THD$Q4.10 == "Yes"), as.numeric(THD$EHF_aware_list))
+
+cor(as.numeric(gr$unemp_benefits == "Yes"), as.numeric(gr$ehf_support_exist_num), use = "complete.obs") # Maybe something to look at?
+cor(as.numeric(gr$unemp_benefits == "Yes"), as.numeric(gr$ehf_support_new_num), use = "complete.obs")
+cor(as.numeric(gr$other_welfare == "Yes"), as.numeric(gr$ehf_support_exist_num), use = "complete.obs")
+cor(as.numeric(gr$other_welfare == "Yes"), as.numeric(gr$ehf_support_new_num), use = "complete.obs")
+
+cor.test(as.numeric(gr$unemp_benefits == "Yes"), as.numeric(gr$ehf_support_exist_num), use = "complete.obs")
+cor.test(as.numeric(gr$unemp_benefits == "Yes"), as.numeric(gr$ehf_support_new_num), use = "complete.obs")
+cor.test(as.numeric(gr$other_welfare == "Yes"), as.numeric(gr$ehf_support_exist_num), use = "complete.obs")
+cor.test(as.numeric(gr$other_welfare == "Yes"), as.numeric(gr$ehf_support_new_num), use = "complete.obs")
+
+
+## Plots
+ggplot(wf) +
+  geom_bar(aes(x = unemp_benefits, fill = ehf_aware_pretr), position = "fill") +
+  facet_grid(cols = vars(og))
+
+ggplot(filter(wf, og == "General Retail")) +
+  geom_bar(aes(x = unemp_benefits, fill = supp), position = "fill") +
+  facet_grid(cols = vars(new))
+  
+ggplot(filter(wf, og == "General Retail" & other_welfare != "Don't know")) +
+  geom_bar(aes(x = other_welfare, fill = supp), position = "fill") +
+  facet_grid(cols = vars(new))
+
+## Use of welfare
+weighted.mean(ACNT$unemp_benefits == "Yes", ACNT$wgt, na.rm = T) # 7.3%
+weighted.mean(gr$unemp_benefits == "Yes", gr$wgt, na.rm = T) # 7.4%
+weighted.mean(THD$unemp_benefits == "Yes", THD$wgt, na.rm = T) # 16.5%
+
+weighted.mean(ACNT$other_welfare == "Yes", ACNT$wgt) # 34.5%
+weighted.mean(gr$other_welfare == "Yes", gr$wgt) # 31.3%
+weighted.mean(THD$other_welfare == "Yes", THD$wgt, na.rm = T) # 19.5%
+
+weighted.mean(ACNT$other_welfare == "Yes" | ACNT$unemp_benefits == "Yes", ACNT$wgt) # 36.7%
+weighted.mean(gr$other_welfare == "Yes"| gr$unemp_benefits == "Yes", gr$wgt) # 35.2%
+weighted.mean(THD$other_welfare == "Yes"| THD$unemp_benefits == "Yes", THD$wgt, na.rm = T) # 30.9%
+
+
+
+
+
+
