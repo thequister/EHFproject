b11dfea9c9783bd6404bc0dd92ecdd97d4880c77
Theodoros Ntounias <tntounias@gmail.com>
2025-08-08 15:24:43 -0500

    model table addition

--- 2_code/Social_Insurance/0_initial_models.R
+++ 2_code/Social_Insurance/0_initial_models.R
@@ -5,49 +5,74 @@ here::i_am("2_code/Social_Insurance/0_initial_models.R")
 ## Read in the data
 gr_clean <- read.csv(here("3_cleaned_data", "general_retail_clean.csv"))
 
+gr_clean$ehf_donate_exist_num <- gr_clean$ehf_donate_exist == "Yes"
+note2 <- "Standard errors in parentheses."
+
 ### General Population Models --------
 
-## EHF Support
-summary(with(gr_clean, lm(ehf_support_new_num ~ replacement_2024_ui + recipiency_2024_ui + WG_TANF)))
+create_mods_gr <- function(gr_data){
+  dv <- with(gr_data, list(
+              ehf_support_exist_num, 
+              ehf_donate_exist_num))
+  
+  iv <- with(gr_data, list(
+                replacement_2024_ui, 
+                recipiency_2024_ui, 
+                WG_TANF))
+  
+  dv_names_main <- c("support for new EHF", "willingness to donate to a new EHF")
+  iv_names_main <- c("replacement rate", "recipiency rate", "tanf generosity")
+  
+  modsums_out <- list()
+  
+  for(i in 1:length(dv)){
+    for(j in 1:length(iv)){
+      ## Without covs
+      gr_ncov <- with(gr_data, lm(dv[[i]] ~ iv[[j]]))
+      
+      ## With covs
+      gr_cov <- with(gr_clean, lm(dv[[i]] ~ iv[[j]]
+                                  + income_num + (home_ownership == "Own") + hpi_5year +
+                                    (other_welfare == "Yes") + ideology_conlib_num + age_clean + male +
+                                    college + practice_religion_num + nonwhite))
+      
+      ## With interaction
+      gr_int <- with(gr_clean, lm(dv[[i]] ~ iv[[j]] + iv[[j]]:practice_religion_num + 
+                                         iv[[j]]:ideology_conlib_num + hpi_5year*(home_ownership == "Own") +
+                                         income_num + (other_welfare == "Yes") + 
+                                         ideology_conlib_num + age_clean + male +
+                                         college + practice_religion_num + nonwhite))
+      
+      coef_maps <- c("iv[[j]]" = iv_names_main[j],
+                     "income_num" = "income",
+                     'home_ownership == "Own"TRUE' = "home owner",
+                     "hpi_5year" = "five year hpi",
+                     'other_welfare == "Yes"TRUE' = "welfare recipient",
+                     "ideology_conlib_num" = "ideology",
+                     "practice_religion_num" = "religiosity",
+                     "maleTRUE" = "male",
+                     "nonwhiteTRUE" = "nonwhite",
+                     "collegeTRUE" = "college", 
+                     "age_clean" = "age",
+                     "iv[[j]]:practice_religion_num" = paste(iv_names_main[j], "x religiosity"), 
+                     "iv[[j]]:ideology_conlib_num" = paste(iv_names_main[j], "x ideology"), 
+                     'hpi_5year:home_ownership == "Own"TRUE' = "home ownership x hpi"
+                     )
+      
+      support.models_gr <- list(gr_ncov, gr_cov, gr_int)
+      
+      names(support.models_gr) <- c("No Covariates", "Covariates", "With Interaction")
+      
+      modsums_out <- append(modsums_out, modelsummary::modelsummary(support.models_gr,
+                                 coef_map = coef_maps,
+                                 vcov = ~residence,
+                                 #add_rows = rows,
+                                 title = paste("OLS regression of", dv_names_main[[i]], "on", iv_names_main[[j]]),
+                                 output = "html",
+                                 notes = list(note2),
+                                 stars = c('*' = .05, '**' = .01)))
+    }
+  }
+  modsums_out
+}
 
-ehf_supp_mod <- with(gr_clean, lm(ehf_support_new_num ~ replacement_2024_ui + recipiency_2024_ui + WG_TANF
-                                  + income + home_ownership +
-                    other_welfare + ideology_conlib_num + age_clean + male +
-                    college + practice_religion_num + nonwhite))
-
-with(gr_clean, lm(ehf_support_new_num ~ replacement_2024_ui + replacement_2024_ui:practice_religion_num + 
-                            replacement_2024_ui:ideology_conlib_num +
-                            income + home_ownership +
-                            other_welfare + ideology_conlib_num + age_clean + male +
-                            college + practice_religion_num + nonwhite))
-
-modelsummary(ehf_supp_mod,
-             title = "Support for Unemployment Insurance",
-             vcov = "robust",
-             threeparttable=TRUE,
-             stars = c('*' = .05, '**' = .01),
-             escape = FALSE
-)
-
-## EHF Donation
-gr_clean$ehf_donate_new_fac <- factor(gr_clean$ehf_donate_new, levels =
-                                        c("I would not donate at all",
-                                          "A one-time donation of less than $20",
-                                          "A one-time donation of more than $20",
-                                          "A regular donation of less than $20",
-                                          "A regular donation of more than $20"),
-                                      ordered = T)
-
-gr_clean$ehf_donate_new_num <- (as.numeric(gr_clean$ehf_donate_new_fac) - 1)/4
-
-summary(with(gr_clean, lm(ehf_donate_new_num ~ replacement_2024_ui)))
-
-summary(with(gr_clean, lm(ehf_donate_new_num ~ replacement_2024_ui + income + home_ownership +
-                            other_welfare + ideology_conlib_num + age_clean + male +
-                            college + practice_religion_num + nonwhite)))
-
-summary(with(gr_clean, lm(ehf_donate_new_num ~ replacement_2024_ui + replacement_2024_ui:practice_religion_num + 
-                            replacement_2024_ui:ideology_conlib_num +
-                            income + home_ownership +
-                            other_welfare + ideology_conlib_num + age_clean + male +
-                            college + practice_religion_num + nonwhite)))
